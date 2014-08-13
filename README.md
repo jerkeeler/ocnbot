8fb9027d0d9b9a960cba3ac583f877b736385b6d
jermkeel <jj.of.keeler@gmail.com>
2014-08-13 13:36:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OCNBot | An irc bot for OCN streamers
+
+This is an IRC bot that is currently being developed by Jeremy K. (CacklingPanda) for the use of Overcast Network (http://oc.tc) streamers on the Twitch (http://twitch.tv) video streaming platform.
---
+++ ocnbot_router.rb
@@ -0,0 +1,48 @@
+require 'cinch'
+
+# Twitch IRC parameters
+host = "irc.twitch.tv"
+user = "OCNBot"
+nick = "OCNBot"
+port = 6667
+channels = ["#cacklingpanda"]
+
+# Filepath to the password file
+pass_file = "pass.secret"
+
+# Simple method for reading the OAutho password from a file
+def secure_pass(filepath)
+	pass_file = filepath
+	if File.exist?(pass_file)
+		File.read(pass_file).chomp
+	else
+		abort("No password file found!")
+	end
+end
+
+password = secure_pass(pass_file)
+
+# Add twitch method to message object to allow the bot to send messages to twitch
+class Cinch::Message
+    def twitch(string)
+        string = string.to_s.gsub('<','&lt;').gsub('>','&gt;')
+        bot.irc.send ":#{bot.config.user}!#{bot.config.user}@#{bot.config.user}.tmi.twitch.tv PRIVMSG #{channel} :#{string}"
+    end
+end
+
+# Create a Cinch bot
+bot = Cinch::Bot.new do
+	configure do |c|
+		c.server = host
+		c.user = user
+		c.nick = nick
+		c.password = password
+		c.channels = channels
+	end
+	
+	on :message, "hello" do |m|
+		m.twitch "Hello #{m.user.nick}!"
+	end
+end
+
+bot.start
