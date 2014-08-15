34534ba2669a7abb92aba95eb754c3dcf6118aed
jermkeel <jj.of.keeler@gmail.com>
2014-08-15 13:53:04 -0400

    Add utilities class and start implementing Player plugin

--- plugins/Player.rb
+++ plugins/Player.rb
@@ -2,22 +2,34 @@
 # command !player <name> is given. The stats format should be displayed like 
 # "PlayerName: Kills - 11, Deaths - 11, KD - 1, KK - 2, Wools - 1, Cores - 1, Monuments - 1, Playtime - 1 hour"
 
-require 'net/https'
-require 'uri'
+require 'nokogiri'
+require_relative 'utilities'
 
 class Player
   include Cinch::Plugin
+  include Utility
 
   # Match the command "!player <name>" and execute the desired actions
   match /player (.+)/
   def execute(m, nick)
-    message = retrieve_player_data(nick)
+    message = retrieve_player_data_string(nick)
     m.twitch(message)
   end
 
   # Retrieve the player data from https://oc.tc given the player's Minecraft
   # name
-  def retrieve_player_data(nick)
-    # TODO: Implement code
+  def retrieve_player_data_string(nick)
+    res = Utility::retrieve_html("https://oc.tc/#{nick}/")
+    case res
+    when Net::HTTPSuccess
+      # parse link
+      doc = Nokogiri::HTML(res.body)
+      #doc.css('h2')
+      "Successfully retrieved html!"
+    when Net::HTTPRedirection
+      "Redirected..."
+    else
+      "That player has not played on OCN."
+    end
   end
 end
---
+++ plugins/utilities.rb
@@ -0,0 +1,17 @@
+# This is a utilities class that has a variety of useful methods that can be 
+# used in a variety of different cases. This is used extensively throughout 
+# the ocnbot's code in order to retrieve html data from https://oc.tc
+
+module Utility
+  require 'net/https'
+
+  # Retrieve html from the specified url, works with SSL web pages
+  def Utility.retrieve_html(url)
+    url = URI.parse(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.port == 443
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.port == 443
+    path = url.path
+    res = http.get(path)
+  end
+end
